a587e397739861ed39f01cd8f271495557d42986
Iamdachi04 <dachi4github@gmail.com>
2025-08-04 17:51:46 +0400

    added the form visibility and functions

--- progress-steps-app/src/components/ProgressApp.tsx
+++ progress-steps-app/src/components/ProgressApp.tsx
@@ -1,5 +1,38 @@
+import React , { useEffect, useState } from "react";
+
 interface Submission {
  name: string;
  surname: string;
 }
 
+
+const [isFormVisible, setIsFormVisible] = useState(true);
+
+const ProgressApp: React.FC = () => {
+const [submissions, setSubmissions] = useState<Submission[]>(() => {
+     const savedSubmissions = localStorage.getItem('submissions');
+     return savedSubmissions ? JSON.parse(savedSubmissions) : [];
+});
+
+useEffect(() => {
+    localStorage.setItem('submissions', JSON.stringify(submissions));
+}, [submissions]);
+
+
+const handleAddSubmission = (newSubmission:Submission)=>{
+    setSubmissions([...submissions, newSubmission]);
+    setIsFormVisible(false);
+}
+
+const handleStartNewForm = () => {
+    setIsFormVisible(true);
+};
+
+return (
+    <div className="progress-app-container">
+        <h2>hulti - step form</h2>
+    </div>
+)
+};
+
+export default ProgressApp;
